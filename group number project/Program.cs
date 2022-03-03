bf6386c767c2256936241620b299fcca6fa043b8
Zakary Kafkis <kaf0003@nhs.vic.edu.au>
2022-03-03 11:43:32 +1100

    Add project files.

---
+++ group number project/Program.cs
@@ -0,0 +1,17 @@
+﻿using System
+
+    namespace GroupNumberProject
+{
+        Console Writeline("guess the number");
+    
+    Random rand = new Random();
+    
+    int number = rand.Next(0, 101); 
+    
+    guess = Console.ReadLine();
+        guess = Convert.ToInt32(Console.ReadLine());
+
+}
+ 
+
+  
